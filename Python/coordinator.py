fe768c4df58fc28dac8ecd62b19a4342901a333b
Erik Laurin <guslauer@student.gu.se>
2019-04-23 15:28:35 +0200

    changed GA library, integrated h264 with a few params

--- Python/coordinator.py
+++ Python/coordinator.py
@@ -1,38 +1,37 @@
 import docker
 import threading
-#import plot_generator
 import sys
 import os
-import numpy as np
-from evolutionary_search import maximize
 import csv
+import time
+from skopt import gp_minimize
+from skopt.space import Integer
+from skopt.utils import use_named_args
+import matplotlib.pyplot as plt
 
 REPO_FFE = 'https://github.com/chrberger/frame-feed-evaluator.git'
-REPO_X264 = 'https://github.com/chalmers-revere/opendlv-video-x264-encoder.git'
+REPO_X264_FULL = 'https://github.com/guslauer/opendlv-video-x264-encoder.git'
 REPO_H264 = 'https://github.com/chalmers-revere/opendlv-video-h264-encoder.git'
-REPO_H264_FULL = 'https://github.com/guslauer/opendlv-video-h264-encoder.git'
 REPO_VPX_FULL = 'https://github.com/jeberlen/opendlv-video-vpx-encoder.git'
 
 VERSION_FFE = 'v0.0.4'
 TAG_FFE = 'ffe:' + VERSION_FFE
 PREFIX_COLOR_FFE = '92'
 
-'''
-VERSION_ENCODER = 'v0.0.7'
-TAG_ENCODER = 'x264:' + VERSION_ENCODER
-PREFIX_COLOR_ENCODER = '94'
-'''
-VERSION_ENCODER = 'v0.0.1'
+VERSION_ENCODER = 'v0.0.2'
 TAG_ENCODER = 'h264:' + VERSION_ENCODER
 PREFIX_COLOR_ENCODER = '94'
 
 '''
-VERSION_ENCODER = 'latest'
-TAG_ENCODER = 'h264-full:' + VERSION_ENCODER
+VERSION_ENCODER = 'v0.0.7'
+TAG_ENCODER = 'x264:' + VERSION_ENCODER
 PREFIX_COLOR_ENCODER = '94'
-'''
 
-'''
+VERSION_ENCODER = 'v0.0.7'
+TAG_ENCODER = 'x264-full:' + VERSION_ENCODER
+PREFIX_COLOR_ENCODER = '94'
+
+
 VERSION_ENCODER = 'latest'
 TAG_ENCODER = 'vpx-full:' + VERSION_ENCODER
 PREFIX_COLOR_ENCODER = '94'
@@ -56,38 +55,38 @@ RESOLUTIONS = [['VGA', '640', '480'], ['SVGA', '800', '600'], ['XGA', '1024', '7
 # http://doxygen.db48x.net/mozilla/html/structvpx__codec__enc__cfg.html
 # Parameters according to https://www.webmproject.org/docs/encoder-parameters/
 
-gop = 1 # 1 - n of frames
+# gop = 1 # 1 - n of frames
 
-g_timebase_num = 1 # Unsure if we need to change
-g_timebase_den = 20 # Unsure if we need to change
-g_threads = 1 # 1 - 4
-g_profile = 0 # 0 - 1
-g_lag_in_frames = 0 # 0 - 25 (over 12 turns on alt-ref frame, a VPx quality enhancer)
+g_timebase_num = 1  # Unsure if we need to change
+g_timebase_den = 20  # Unsure if we need to change
+g_threads = 1  # 1 - 4
+g_profile = 0  # 0 - 1
+g_lag_in_frames = 0  # 0 - 25 (over 12 turns on alt-ref frame, a VPx quality enhancer)
 
-rc_dropframe_thresh = 0.0 # 0.0 - 1.0
-rc_resize_allowed = 0 # 0 - 1
-rc_resize_up_thresh = 0.0 # 0.0 - 1.0
-rc_resize_down_thresh = 0.0 # 0.0 - 1.0
-rc_undershoot_pct = 0 # 0 - 100 | VP8 ? 0 - 1000
-rc_overshoot_pct = 0 # 0 - 100 | VP8 ? 0 - 1000
-rc_min_quantizer = 0 # 0 - 52 | VP8 ? 0 - 56
-rc_max_quantizer = 0 # 0 - 52 | VP8 ? 0 - 56
-rc_end_usage = 0 # 0 - 1
-rc_buf_sz = 0 # 0 - 60000
-rc_buf_initial_sz = 0 # 0 - 40000
-rc_buf_optimal_sz = 0 # 0 - 50000
+rc_dropframe_thresh = 0.0  # 0.0 - 1.0
+rc_resize_allowed = 0  # 0 - 1
+rc_resize_up_thresh = 0.0  # 0.0 - 1.0
+rc_resize_down_thresh = 0.0  # 0.0 - 1.0
+rc_undershoot_pct = 0  # 0 - 100 | VP8 ? 0 - 1000
+rc_overshoot_pct = 0  # 0 - 100 | VP8 ? 0 - 1000
+rc_min_quantizer = 0  # 0 - 52 | VP8 ? 0 - 56
+rc_max_quantizer = 0  # 0 - 52 | VP8 ? 0 - 56
+rc_end_usage = 0  # 0 - 1
+rc_buf_sz = 0  # 0 - 60000
+rc_buf_initial_sz = 0  # 0 - 40000
+rc_buf_optimal_sz = 0  # 0 - 50000
 rc_target_bitrate = 100000  # 100000 - 5000000
 
-kf_mode = 0 # 0 - 1
-kf_min_dist = 0 # 0 - n of frames
-kf_max_dist = 0 # 0 - n of frames
+kf_mode = 0  # 0 - 1
+kf_min_dist = 0  # 0 - n of frames
+kf_max_dist = 0  # 0 - n of frames
 
 ######################
 # https://www.web3.lu/avc-h264-video-settings/ # https://www.pixeltools.com/rate_control_paper.html
 # https://superuser.com/questions/638069/what-is-the-maximum-and-minimum-of-q-value-in-ffmpeg
 # https://slhck.info/video/2017/02/24/crf-guide.html
 
-rc_mode = 0  # -1 -3
+# rc_mode = 0  # -1 - 3
 ecomplexity = 0  # 0 - 2
 num_ref_frame = 1  # 1 - 16
 sps_pps_strategy = 0  # 0, 1, 2, 3, 6
@@ -106,8 +105,10 @@ b_background_detection = 0  # 0, 1
 b_adaptive_quant = 0  # 0, 1
 b_frame_cropping = 0  # 0, 1
 b_scene_change_detect = 0  # 0, 1
-#bitrate = 100000  # 100,000 - 5,000,000
-gop = 1
+
+
+# bitrate = 100000  # 100,000 - 5,000,000
+# gop = 1
 
 
 ######################
@@ -131,13 +132,15 @@ VOLUMES_FFE = {'/tmp/': {'bind': '/tmp', 'mode': 'rw'},
                PNGS_PATH: {
                    'bind': '/pngs',
                    'mode': 'rw'}
+               }
+
 
 def get_list_encoder_vp9_full():
     return ['--cid=' + CID,
-            '--name=' + NAME,
+            '--name=' + SHARED_MEMORY_AREA,
             '--width=' + width,
             '--height=' + height,
-            #'--verbose',
+            # '--verbose',
             ###################
             '--gop=' + str(gop),
             '--threads=' + str(g_threads),
@@ -161,6 +164,7 @@ def get_list_encoder_vp9_full():
             '--kf-max-dist=' + str(kf_max_dist)
             ]
 
+
 def get_list_encoder_h264_full():
     return ['--cid=' + CID,
             '--name=' + SHARED_MEMORY_AREA,
@@ -168,6 +172,10 @@ def get_list_encoder_h264_full():
             '--height=' + height,
             # '--verbose,',
             ###################
+
+            '--bitrate=' + str(bitrate),
+            '--bitrate=' + str(bitrate),
+
             '--rc-mode=' + str(rc_mode),
             '--ecomplexity=' + str(ecomplexity),
             '--num-ref-frame=' + str(num_ref_frame),
@@ -188,7 +196,6 @@ def get_list_encoder_h264_full():
             '--frame-cropping=' + str(b_frame_cropping),
             '--scene-change-detect=' + str(b_scene_change_detect),
             '--gop=' + str(gop),
-            '--bitrate=' + str(bitrate)
             ]
 
 
@@ -261,22 +268,33 @@ def main(docker_client):
 
 #    plot_generator.run(reports, OUTPUT_GRAPH_PATH)
 
-def f(bitrate):
-    print('SET BITRATE FROM GA: ' + str(bitrate))
+param_grid0 = [Integer(100000, 1500000, name='bitrate'),
+               Integer(0, 1, name='entropy_coding'),
+               Integer(0, 1, name='denoise'),
+               Integer(0, 2, name='ecomplexity'),
+               Integer(0, 1, name='adaptive_quant')
+               ]
+
+
+@use_named_args(param_grid0)
+def f(bitrate, denoise, entropy_coding, adaptive_quant, ecomplexity):
+    # print('bitrate: ' + str(bitrate))# + '\ndenoise: ' + str(denoise) +'\nentropy_coding: ' + str(entropy_coding))
+
     def get_list_ffe():
         return ['--folder=/pngs',
-                '--report=' + str(bitrate) +'.csv',
+                '--report=' + str(bitrate) + '.csv',
                 '--cid=' + CID,
                 '--name=' + SHARED_MEMORY_AREA,
                 '--crop.x=' + calculate_crop_x(),
                 '--crop.y=' + calculate_crop_y(),
                 '--crop.width=' + width,
                 '--crop.height=' + height,
-                '--noexitontimeout',
                 '--delay=0',
-                '--delay.start=1000',
-                '--stopafter=15',
+                '--delay.start=150',
+                '--stopafter=40',
+                #'--noexitontimeout'
                 #'--verbose',
+                '--timeout=100'
                 ]
 
     def get_list_encoder():
@@ -284,8 +302,13 @@ def f(bitrate):
                 '--name=' + SHARED_MEMORY_AREA,
                 '--width=' + width,
                 '--height=' + height,
+                '--rc-mode=1',
                 '--bitrate=' + str(bitrate),
-                #'--verbose'
+                '--denoise' + str(denoise),
+                '--entropy-coding' + str(entropy_coding),
+                '--adaptive_quant' + str(adaptive_quant),
+                '--ecomplexity' + str(ecomplexity),
+               # '--verbose'
                 ]
 
     container_ffe = docker_client.containers.run(TAG_FFE,
@@ -325,19 +348,42 @@ def f(bitrate):
     for row in plots:
         sum += float(row[10])
         length += 1
+
+    if length < 15:
+        print("length only :" + str(length))
+        return 1
     avg = sum / length
-    print(avg)
-    return avg
+    return 1 - avg
 
 
 if __name__ == '__main__':
     docker_client = docker.from_env()
     main(docker_client)
 
-    paramgrid = {"bitrate": np.arange(100000, 1500000, 100000)}
-    best_params, best_ssim, score_results, _, _ = maximize(func=f, parameter_dict=paramgrid, verbose=False,
-                                                           population_size=5, tournament_size=2, generations_number=3)
+    start0 = time.time()
 
-    print(best_params)
-    #print(best_ssim)
-    print(score_results)
+    res = gp_minimize(func=f, dimensions=param_grid0, base_estimator=None,
+                      n_calls=10, n_random_starts=10,
+                      acq_func="gp_hedge", acq_optimizer="auto", x0=None, y0=None,
+                      random_state=None, verbose=True, callback=None,
+                      n_points=10000, n_restarts_optimizer=5, xi=0.01, kappa=1.96,
+                      noise="gaussian", n_jobs=1, )
+    now0 = time.time()
+
+    print("----------minimize-----------")
+    print("Best score=%.4f" % res.fun)
+    print("""Best parameters:
+    - bitrate: %d
+    - entropy_coding: %d
+    - denoise: %d
+    - ecomplexity: %d
+    - adaptive_quant: %d"""
+          % (res.x[0], res.x[1], res.x[2], res.x[3], res.x[4]))
+
+    from skopt.plots import plot_convergence
+
+    plot_convergence(res)
+    plt.show()
+
+    print("It took: ", now0 - start0, " seconds")
+    print("Or : ", (now0 - start0) / 60, " minutes")
