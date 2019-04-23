dd7bb062ec1e6261f6d2c7ed6ba67f249da7f938
Erik Laurin <guslauer@student.gu.se>
2019-04-24 00:38:17 +0200

    added all h264 params and resolutions to script

--- Python/coordinator.py
+++ Python/coordinator.py
@@ -5,9 +5,11 @@ import os
 import csv
 import time
 from skopt import gp_minimize
-from skopt.space import Integer
+from skopt.space import Integer, Categorical
 from skopt.utils import use_named_args
+from skopt.plots import plot_convergence
 import matplotlib.pyplot as plt
+import plot_generator
 
 REPO_FFE = 'https://github.com/chrberger/frame-feed-evaluator.git'
 REPO_X264_FULL = 'https://github.com/guslauer/opendlv-video-x264-encoder.git'
@@ -39,11 +41,18 @@ PREFIX_COLOR_ENCODER = '94'
 
 width = '640'
 height = '480'
+resolution = 'not_set'
 
-report_name = 'test'
+best_config = 'not_set'
+config = 0
+max_ssim = 0
 
 INITIAL_WIDTH = '2048'
 INITIAL_HEIGHT = '1536'
+
+STOP_AFTER = 80
+TIMED_OUT_MSG_BYTES = str.encode('[frame-feed-evaluator]: Timed out while waiting for encoded frame.\n')
+TIMED_OUT = 0
 
 CID = '112'
 SHARED_MEMORY_AREA = 'video1'
@@ -80,35 +89,6 @@ rc_target_bitrate = 100000  # 100000 - 5000000
 kf_mode = 0  # 0 - 1
 kf_min_dist = 0  # 0 - n of frames
 kf_max_dist = 0  # 0 - n of frames
-
-######################
-# https://www.web3.lu/avc-h264-video-settings/ # https://www.pixeltools.com/rate_control_paper.html
-# https://superuser.com/questions/638069/what-is-the-maximum-and-minimum-of-q-value-in-ffmpeg
-# https://slhck.info/video/2017/02/24/crf-guide.html
-
-# rc_mode = 0  # -1 - 3
-ecomplexity = 0  # 0 - 2
-num_ref_frame = 1  # 1 - 16
-sps_pps_strategy = 0  # 0, 1, 2, 3, 6
-b_prefix_nal = 0  # 0, 1
-b_ssei = 0  # 0, 1
-i_padding = 0  # 0, 1
-i_entropy_coding = 0  # 0, 1
-b_frame_skip = 0  # 0, 1
-i_bitrate_max = 5000000  # -5,000,000
-i_max_qp = 51  # 0 - 51 def: 42 // reasonable: 32 - 51?
-i_min_qp = 18  # 0 - 50 def: 12 // reasonable: 7 - 17?
-i_long_term_reference = 0  # 0, 1
-i_loop_filter = 0  # 0, 1, 2
-b_denoise = 0  # 0, 1
-b_background_detection = 0  # 0, 1
-b_adaptive_quant = 0  # 0, 1
-b_frame_cropping = 0  # 0, 1
-b_scene_change_detect = 0  # 0, 1
-
-
-# bitrate = 100000  # 100,000 - 5,000,000
-# gop = 1
 
 
 ######################
@@ -165,44 +145,13 @@ def get_list_encoder_vp9_full():
             ]
 
 
-def get_list_encoder_h264_full():
-    return ['--cid=' + CID,
-            '--name=' + SHARED_MEMORY_AREA,
-            '--width=' + width,
-            '--height=' + height,
-            # '--verbose,',
-            ###################
-
-            '--bitrate=' + str(bitrate),
-            '--bitrate=' + str(bitrate),
-
-            '--rc-mode=' + str(rc_mode),
-            '--ecomplexity=' + str(ecomplexity),
-            '--num-ref-frame=' + str(num_ref_frame),
-            '--sps-pps=' + str(sps_pps_strategy),
-            '--prefix-nal=' + str(b_prefix_nal),
-            '--ssei=' + str(b_ssei),
-            '--padding=' + str(i_padding),
-            '--entropy-coding=' + str(i_entropy_coding),
-            '--frame-skip=' + str(b_frame_skip),
-            '--bitrate-max=' + str(i_bitrate_max),
-            '--qp-max=' + str(i_max_qp),
-            '--qp-min=' + str(i_min_qp),
-            '--long-term-ref=' + str(i_long_term_reference),
-            '--loop-filter=' + str(i_loop_filter),
-            '--denoise=' + str(b_denoise),
-            '--background-detection=' + str(b_background_detection),
-            '--adaptive-quant=' + str(b_adaptive_quant),
-            '--frame-cropping=' + str(b_frame_cropping),
-            '--scene-change-detect=' + str(b_scene_change_detect),
-            '--gop=' + str(gop),
-            ]
-
-
-# Iterates the generator and prints its log
-def print_logs(log_generator, color):
+# Iterates the generator and prints its log and sets violation TIMED_OUT
+def log_helper(log_generator, color):
     for x in log_generator:
         print('\033[' + color + 'm### \033[0m' + (str(x, 'utf-8')))  # Prints with color code prefix and in utf-8
+        if x == TIMED_OUT_MSG_BYTES:
+            global TIMED_OUT
+            TIMED_OUT = 1
 
 
 ########################################################################################################################
@@ -255,34 +204,56 @@ def main(docker_client):
     thread_build_ffe.join()  # Blocks execution until both threads has terminated
     thread_build_encoder.join()
 
-    # reports = []
-    # for (i, res) in enumerate(RESOLUTIONS):
-    #     report_name = 'ffe-AstaZero_Rural_Road-' + TAG_ENCODER + '-' + res[0] + '-  .csv'
-    #     reports.append('reports/' + report_name)
-    #     width = res[1]
-    #     height = res[2]
-    #
-    #     print(get_list_ffe())
-    #     print(get_list_encoder())
+
+# https://www.web3.lu/avc-h264-video-settings/ # https://www.pixeltools.com/rate_control_paper.html
+# https://superuser.com/questions/638069/what-is-the-maximum-and-minimum-of-q-value-in-ffmpeg
+# https://slhck.info/video/2017/02/24/crf-guide.html
+# All parameters available in h264 and their ranges
+space_h264 = [Integer(100000, 1500000, name='bitrate'),
+              Integer(100000, 1500000, name='bitrate_max'),
+              Integer(1, 250, name='gop'),
+              Integer(-1, 3, name='rc_mode'),
+              Integer(0, 2, name='ecomplexity'),
+              Categorical((0, 1, 2, 3, 6), name='sps_pps_strategy'),
+              Integer(1, 16, name='num_ref_frame'),
+              Integer(0, 1, name='ssei'),
+              Integer(0, 1, name='prefix_nal'),
+              Integer(0, 1, name='entropy_coding'),
+              Integer(0, 1, name='frame_skip'),
+              Integer(32, 51, name='qp_max'),  # change
+              Integer(7, 17, name='qp_min'),  # change
+              Integer(0, 1, name='long_term_ref'),
+              Integer(0, 2, name='loop_filter'),
+              Integer(0, 1, name='denoise'),
+              Integer(0, 1, name='background_detection'),
+              Integer(0, 1, name='adaptive_quant'),
+              Integer(0, 1, name='frame_cropping'),
+              Integer(0, 1, name='scene_change_detect'),
+              Integer(0, 1, name='padding')
+              ]
+
+param_list_h264 = (
+    'bitrate', 'bitrate_max', 'gop', 'rc_mode', 'ecomplexity', 'sps_pps_strategy', 'num_ref_frame', 'ssei',
+    'prefix_nal', 'entropy_coding', 'frame_skip', 'qp_max', 'qp_min', 'long_term_ref', 'loop_filter', 'denoise',
+    'background_detection', 'adaptive_quant', 'frame_cropping', 'scene_change_detect', 'padding'
+)
 
 
-#    plot_generator.run(reports, OUTPUT_GRAPH_PATH)
+@use_named_args(space_h264)
+def objective_h264(bitrate, bitrate_max, gop, rc_mode, ecomplexity, sps_pps_strategy, num_ref_frame, ssei, prefix_nal,
+                   entropy_coding, frame_skip, qp_max, qp_min, long_term_ref, loop_filter, denoise,
+                   background_detection, adaptive_quant, frame_cropping, scene_change_detect, padding):
+    # update report name with this iteration's config
+    global config
+    report_name = 'ffe-AstaZero_Rural_Road-' + TAG_ENCODER + '-' + res[0] + '-' + 'C' + str(config) + '.csv'
+    config += 1
 
-param_grid0 = [Integer(100000, 1500000, name='bitrate'),
-               Integer(0, 1, name='entropy_coding'),
-               Integer(0, 1, name='denoise'),
-               Integer(0, 2, name='ecomplexity'),
-               Integer(0, 1, name='adaptive_quant')
-               ]
-
-
-@use_named_args(param_grid0)
-def f(bitrate, denoise, entropy_coding, adaptive_quant, ecomplexity):
-    # print('bitrate: ' + str(bitrate))# + '\ndenoise: ' + str(denoise) +'\nentropy_coding: ' + str(entropy_coding))
+    global TIMED_OUT
+    TIMED_OUT = 0  # resets violation variable
 
     def get_list_ffe():
         return ['--folder=/pngs',
-                '--report=' + str(bitrate) + '.csv',
+                '--report=' + report_name,
                 '--cid=' + CID,
                 '--name=' + SHARED_MEMORY_AREA,
                 '--crop.x=' + calculate_crop_x(),
@@ -291,10 +262,9 @@ def f(bitrate, denoise, entropy_coding, adaptive_quant, ecomplexity):
                 '--crop.height=' + height,
                 '--delay=0',
                 '--delay.start=150',
-                '--stopafter=40',
-                #'--noexitontimeout'
-                #'--verbose',
-                '--timeout=100'
+                '--stopafter=' + str(STOP_AFTER),
+                # '--noexitontimeout'
+                # '--verbose',
                 ]
 
     def get_list_encoder():
@@ -304,86 +274,126 @@ def f(bitrate, denoise, entropy_coding, adaptive_quant, ecomplexity):
                 '--height=' + height,
                 '--rc-mode=1',
                 '--bitrate=' + str(bitrate),
-                '--denoise' + str(denoise),
+                '--bitrate-max=' + str(bitrate_max),
+                '--gop=' + str(gop),
+                '--rc-mode=' + str(rc_mode),
+                '--ecomplexity=' + str(ecomplexity),
+                '--sps_pps_strategy=' + str(sps_pps_strategy),
+                '--num_ref_frame=' + str(num_ref_frame),
+                '--ssei=' + str(ssei),
+                '--prefix-nal=' + str(prefix_nal),
                 '--entropy-coding' + str(entropy_coding),
+                '--frame-skip=' + str(frame_skip),
+                '--qp-max=' + str(qp_max),
+                '--qp-min=' + str(qp_min),
+                '--long-term-ref' + str(long_term_ref),
+                '--loop-filter=' + str(loop_filter),
+                '--denoise' + str(denoise),
+                '--background-detection=' + str(background_detection),
                 '--adaptive_quant' + str(adaptive_quant),
-                '--ecomplexity' + str(ecomplexity),
-               # '--verbose'
+                '--frame-cropping' + str(frame_cropping),
+                '--scene-change-detect=' + str(scene_change_detect),
+                '--padding=' + str(padding),
+                # '--verbose'
                 ]
 
-    container_ffe = docker_client.containers.run(TAG_FFE,
-                                                 command=get_list_ffe(),
-                                                 volumes=VOLUMES_FFE,
-                                                 environment=['DISPLAY=:0'],
-                                                 working_dir='/host',
-                                                 network_mode="host",
-                                                 ipc_mode="host",
-                                                 remove=True,
-                                                 detach=True,
-                                                 )
-
-    container_encoder = docker_client.containers.run(TAG_ENCODER,
-                                                     command=get_list_encoder(),
-                                                     volumes={'/tmp': {'bind': '/tmp', 'mode': 'rw'}},
+    try:  # try/catch to catch when the containers crash due to illegal parameter combination
+        container_ffe = docker_client.containers.run(TAG_FFE,
+                                                     command=get_list_ffe(),
+                                                     volumes=VOLUMES_FFE,
+                                                     environment=['DISPLAY=:0'],
+                                                     working_dir='/host',
                                                      network_mode="host",
                                                      ipc_mode="host",
                                                      remove=True,
-                                                     detach=True
+                                                     detach=True,
                                                      )
 
-    thread_logs_ffe = threading.Thread(target=print_logs, args=[container_ffe.logs(stream=True), PREFIX_COLOR_FFE])
-    thread_logs_encoder = threading.Thread(target=print_logs, args=[container_encoder.logs(stream=True),
-                                                                    PREFIX_COLOR_ENCODER])
+        container_encoder = docker_client.containers.run(TAG_ENCODER,
+                                                         command=get_list_encoder(),
+                                                         volumes={'/tmp': {'bind': '/tmp', 'mode': 'rw'}},
+                                                         network_mode="host",
+                                                         ipc_mode="host",
+                                                         remove=True,
+                                                         detach=True
+                                                         )
+        thread_logs_ffe = threading.Thread(target=log_helper, args=[container_ffe.logs(stream=True), PREFIX_COLOR_FFE])
+        thread_logs_encoder = threading.Thread(target=log_helper, args=[container_encoder.logs(stream=True),
+                                                                        PREFIX_COLOR_ENCODER])
+        thread_logs_ffe.start()
+        thread_logs_encoder.start()
 
-    thread_logs_ffe.start()
-    thread_logs_encoder.start()
+        thread_logs_ffe.join()  # Blocks execution until both threads has terminated
+        thread_logs_encoder.join()  # @TODO  Change to have the actual containers, encoder and ffe, blocking
 
-    thread_logs_ffe.join()  # @TODO  Change to have the actual containers, encoder and ffe, blocking
-    thread_logs_encoder.join()
+    except Exception as e:
+        print(e)
+        print("Most likely an illegal encoder config combination")
+        try:
+            container_ffe.kill()  # ensures that both containers are killed to not get conflicts for new containers
+            container_encoder.kill()
+            return 1  # returns 1 as SSIM in case of crash (inverted due to minimization algorithm)
+        except Exception as e:
+            print(e)
+            return 1  # returns 1 as SSIM in case of crash (inverted due to minimization algorithm)
 
-    file = open(OUTPUT_REPORT_PATH + '/' + str(bitrate) + '.csv', 'r')
+    if TIMED_OUT:  # FFE timed out due to size or compression time constraint violated
+        print("TIMED OUT")
+        return 1  # returns 1 (inverted due to minimization algorithm)
+
+    file = open(OUTPUT_REPORT_PATH + '/' + report_name, 'r')  # opens report generated
     plots = csv.reader(file, delimiter=';')
     sum = 0
     length = 0
     for row in plots:
-        sum += float(row[10])
+        sum += float(row[10])  # accomulate values in SSIM column
         length += 1
 
-    if length < 15:
-        print("length only :" + str(length))
-        return 1
-    avg = sum / length
-    return 1 - avg
+    avg = sum / length  # computes SSIM average
+
+    global max_ssim
+    if avg > max_ssim:  # if the new mean ssim is the best so far, update max_ssim and best_config variables
+        max_ssim = avg
+        global best_config
+        best_config = report_name
+
+    return 1 - avg  # subtracts mean SSIM from 1 since the algorithm tries to find the minimum
 
 
 if __name__ == '__main__':
     docker_client = docker.from_env()
     main(docker_client)
 
-    start0 = time.time()
+    reports = []
 
-    res = gp_minimize(func=f, dimensions=param_grid0, base_estimator=None,
-                      n_calls=10, n_random_starts=10,
-                      acq_func="gp_hedge", acq_optimizer="auto", x0=None, y0=None,
-                      random_state=None, verbose=True, callback=None,
-                      n_points=10000, n_restarts_optimizer=5, xi=0.01, kappa=1.96,
-                      noise="gaussian", n_jobs=1, )
-    now0 = time.time()
+    for (i, res) in enumerate(RESOLUTIONS):
+        width = res[1]
+        height = res[2]
+        resolution = RESOLUTIONS[0]
+        max_ssim = 0  # resets max SSIM, config and best config for current resolution
+        best_config = 'not_set'
+        config = 0
 
-    print("----------minimize-----------")
-    print("Best score=%.4f" % res.fun)
-    print("""Best parameters:
-    - bitrate: %d
-    - entropy_coding: %d
-    - denoise: %d
-    - ecomplexity: %d
-    - adaptive_quant: %d"""
-          % (res.x[0], res.x[1], res.x[2], res.x[3], res.x[4]))
+        start = time.time()
+        minimize_results = gp_minimize(func=objective_h264, dimensions=space_h264, base_estimator=None,
+                                       n_calls=100, n_random_starts=10,
+                                       acq_func="gp_hedge", acq_optimizer="auto", x0=None, y0=None,
+                                       random_state=None, verbose=True, callback=None,
+                                       n_points=10000, n_restarts_optimizer=5, xi=0.01, kappa=1.96,
+                                       noise="gaussian", n_jobs=1, )
+        now = time.time()
 
-    from skopt.plots import plot_convergence
+        print("Best score=%.4f" % minimize_results.fun)
+        print("Best parameters:")
+        i = 0
+        for value in minimize_results.x:
+            print(param_list_h264[i] + ': ' + str(value))  # prints parameters that obtained the highest SSIM
+            i += 1
+        print("Best config: " + best_config)
+        print("It took: ", str((now - start) / 60), " minutes")
 
-    plot_convergence(res)
-    plt.show()
+        plot_convergence(minimize_results)
+        plt.show()
+        reports.append('reports/' + best_config)
 
-    print("It took: ", now0 - start0, " seconds")
-    print("Or : ", (now0 - start0) / 60, " minutes")
+    plot_generator.run(reports, OUTPUT_GRAPH_PATH)
