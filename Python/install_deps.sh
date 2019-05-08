9470e52e9178331caf59d6d533baacfc40145d99
Christian Berger <christian.berger@gu.se>
2019-05-08 11:02:07 +0200

    added required python packages

--- Python/install_deps.sh
+++ Python/install_deps.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo apt-get install python3.6 -y
+sudo apt-get install python3.6-tk -y
+sudo apt-get install python3-venv -y
 
 python3 -m venv ~/py3-environments/coordinator
 source ~/py3-environments/coordinator/bin/activate
