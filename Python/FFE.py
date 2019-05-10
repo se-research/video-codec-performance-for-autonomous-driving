1ef1b56f1ea20bd5567317605d85c105c17d4e04
Christian Berger <christian.berger@gu.se>
2019-05-10 18:52:24 +0200

    test

--- Python/FFE.py
+++ Python/FFE.py
@@ -39,9 +39,9 @@ def get_commands():
                 '--crop.width=' + _local_variables['width'],
                 '--crop.height=' + _local_variables['height'],
                 '--delay=0',
-                '--delay.start=500',
+                '--delay.start=1000',
                 #'--stopafter=' + str(utilities.STOP_AFTER),
-                '--timeout=100',
+                '--timeout=100'
                 # '--noexitontimeout'
                 #'--verbose'
                 ]
