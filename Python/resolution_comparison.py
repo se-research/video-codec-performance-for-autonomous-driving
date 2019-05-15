6cabe2bcf9ebf774a86e4e76e7088ec9992b8d42
jeberlen <guseberjo@student.gu.se>
2019-05-15 17:26:09 +0200

    Fixed graphical issues

--- Python/resolution_comparison.py
+++ Python/resolution_comparison.py
@@ -16,11 +16,13 @@ def run(encoder_list):
         resolutions = utilities.RESOLUTIONS
         ind = np.arange(len(resolutions))
 
-        fig, ax = plt.subplots(figsize=(15, 15))
+        fig, ax = plt.subplots(figsize=(15, 15), sharey=True)
+        par1 = ax.twinx()
         encs = {}
         x_positions = {}
         values = []
         keys = []
+        count = 0
         for x in range(len(resolutions)):
             resolution = resolutions[x][0]
             best_configs = []
@@ -48,37 +50,48 @@ def run(encoder_list):
                     if k == H264.TAG:
                         continue
                     try:
-                        per = (v - encs[H264.TAG]) / encs[H264.TAG]
+                        per = ((v - encs[H264.TAG]) / encs[H264.TAG]) * 100
                         keys.append(k + '\n' + resolution)
                         values.append(per)
-                        x_positions[resolution] += 1
+                        if resolution in x_positions:
+                            x_positions[resolution] += 1
+                        else:
+                            x_positions[resolution] = 1
                     except ZeroDivisionError:
                         keys.append(k + '\n' + resolution)
                         values.append(0)
-                        x_positions[resolution] += 1
-        
+                        if resolution in x_positions:
+                            x_positions[resolution] += 1
+                        else:
+                            x_positions[resolution] = 1
 
         # initialize our bar width and the subplot
         width = 0.2
-        rects1 = ax.bar(np.arange(1, len(keys) + 1), height = values, width = width, color='blue')
+        color_list = ['#aa0000', '#00aa00', '#0000aa', '#aa00aa', '#aaaa00', '#aaff00', '#44ff00']
+        color_list = color_list[:len(encs.keys()) -1]
+        rects1 = ax.bar(np.arange(1, len(keys) + 1), height = values, width = width, color=color_list)
         
         # Set our axes labels, title, tick marks, and then our x ticks.
         ax.set_ylabel('SSIM CHANGE')
-        ax.set_ylim(-0.2, 0.2)
+        #ax.set_ylim(-20, 20)
         ax.set_title(dataset)
         ax.set_xticks(np.arange(1, len(keys) + 1))
-        #labels = [item.get_text() for item in ax.get_yticklabels()]
-        #labels[4] = H264.TAG
-        #ax.set_yticklabels(labels)
+
         encs.pop(H264.TAG, None)
         ax.set_xticklabels(keys, rotation = 45, fontsize = 10)
         # Create a horizontal line at the origin
         ax.axhline(y=0, color='black')
         output_path = utilities.get_comparison_output_graph_path()
         plt.tick_params(axis='x', which='major', labelsize=10)
-
-        ax.axvline(x=len(x_positions[resolution]) + 0.5, color='#000000')
-
+        
+        labels = [item.get_text() for item in ax.get_yticklabels()]
+        labels[0] = H264.TAG
+        par1.set_yticklabels(labels)
+        
+        x_positions.popitem()
+        for key in x_positions: 
+            ax.axvline(x=count + x_positions[key] + 0.5, color='#000000')
+            count += x_positions[key]
         plt.gcf().subplots_adjust(bottom=0.15)
         plt.tight_layout()
 
